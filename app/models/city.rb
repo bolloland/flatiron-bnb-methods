04533d391be361dcdbbe863b2a512040d239c7f1
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-06 15:55:43 -0500

    commit tree 8426fe35e7f647181da0cdb66fad34460837e91a

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,12 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  
+  def city_openings(start_date, end_date)
+    Listing.all(conditions: ["created_at >= ? AND created_at <= ?", params[:start_date], params[:end_date]])
+    end
+
+
+
 end
 
